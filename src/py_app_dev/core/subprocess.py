de4960cef867b3978942216db227cfd6a1319439
cuinixam <22344617+cuinixam@users.noreply.github.com>
2025-01-22 14:33:41 +0100

    feat: capture subprocess output

--- src/py_app_dev/core/subprocess.py
+++ src/py_app_dev/core/subprocess.py
@@ -1,7 +1,7 @@
 import shutil
 import subprocess  # nosec
 from pathlib import Path
-from typing import Dict, List, Optional, Union
+from typing import Any, Dict, List, Optional, Union
 
 from .exceptions import UserNotificationException
 from .logging import logger
@@ -14,6 +14,18 @@ def which(app_name: str) -> Optional[Path]:
 
 
 class SubprocessExecutor:
+    """
+    Execute a command in a subprocess.
+
+    Args:
+    ----
+
+        capture_output: If True, the output of the command will be captured.
+        print_output: If True, the output of the command will be printed to the logger.
+                      One can set this to false in order to get the output in the returned CompletedProcess object.
+
+    """
+
     def __init__(
         self,
         command: Union[str, List[str | Path]],
@@ -21,6 +33,7 @@ class SubprocessExecutor:
         capture_output: bool = True,
         env: Optional[Dict[str, str]] = None,
         shell: bool = False,
+        print_output: bool = True,
     ):
         self.logger = logger.bind()
         self.command = command
@@ -28,6 +41,7 @@ class SubprocessExecutor:
         self.capture_output = capture_output
         self.env = env
         self.shell = shell
+        self.print_output = print_output
 
     @property
     def command_str(self) -> str:
@@ -35,8 +49,12 @@ class SubprocessExecutor:
             return self.command
         return " ".join(str(arg) if not isinstance(arg, str) else arg for arg in self.command)
 
-    def execute(self) -> None:
+    def execute(self, handle_errors: bool = True) -> Optional[subprocess.CompletedProcess[Any]]:
+        """Execute the command and return the CompletedProcess object if handle_errors is False."""
         try:
+            completed_process = None
+            stdout = ""
+            stderr = ""
             self.logger.info(f"Running command: {self.command_str}")
             cwd_path = (self.current_working_directory or Path.cwd()).as_posix()
             with subprocess.Popen(
@@ -49,14 +67,23 @@ class SubprocessExecutor:
                 shell=self.shell,
             ) as process:  # nosec
                 if self.capture_output and process.stdout is not None:
-                    for line in iter(process.stdout.readline, ""):
-                        self.logger.info(line.strip())
-                process.wait()
+                    if self.print_output:
+                        for line in iter(process.stdout.readline, ""):
+                            self.logger.info(line.strip())
+                        process.wait()
+                    else:
+                        stdout, stderr = process.communicate()
 
-            # Check return code
-            if process.returncode != 0:
-                raise subprocess.CalledProcessError(process.returncode, self.command_str)
+            if handle_errors:
+                # Check return code
+                if process.returncode != 0:
+                    raise subprocess.CalledProcessError(process.returncode, self.command_str)
+            else:
+                completed_process = subprocess.CompletedProcess(process.args, process.returncode, stdout, stderr)
         except subprocess.CalledProcessError as e:
-            raise UserNotificationException(f"Command '{self.command_str}' failed with return code {e.returncode}") from None
+            raise UserNotificationException(f"Command '{self.command_str}' execution failed with return code {e.returncode}") from None
         except FileNotFoundError as e:
-            raise UserNotificationException(f"Command '{self.command_str}' failed with error {e}") from None
+            raise UserNotificationException(f"Command '{self.command_str}' could not be executed. Failed with error {e}") from None
+        except KeyboardInterrupt:
+            raise UserNotificationException(f"Command '{self.command_str}' execution interrupted by user") from None
+        return completed_process
--- tests/test_subprocess.py
+++ tests/test_subprocess.py
@@ -9,3 +9,12 @@ def test_get_app_path():
 
 def test_subprocess_executor(tmp_path: Path) -> None:
     SubprocessExecutor(["python", "-V"], cwd=tmp_path, capture_output=True).execute()
+
+
+def test_subprocess_executor_no_error_handling() -> None:
+    process = SubprocessExecutor(["python", "-V"], capture_output=True).execute(handle_errors=False)
+    assert process and process.returncode == 0
+    assert process.stdout == ""
+    process = SubprocessExecutor(["python", "-V"], capture_output=True, print_output=False).execute(handle_errors=False)
+    assert process and process.returncode == 0
+    assert "Python" in process.stdout
